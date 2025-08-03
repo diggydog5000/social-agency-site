a106ef148dbde2bab01804a56c7427f99ecdf0f1
diggydog5000 <howardbhenry2@gmail.com>
2025-08-02 22:20:55 -0400

    Latest

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -1,14 +1,35 @@
+// src/components/ui/Navbar.js
+
 import React from 'react';
 
 export function Navbar() {
   return (
     <nav className="fixed top-0 left-0 w-full flex items-center justify-between p-6 z-20 bg-transparent">
-      <div className="text-white font-bold text-xl">Social House CT</div>
+      {/* Logo on the left */}
+      <div className="flex-shrink-0">
+        <a href="/">
+          <img
+            src="/assets/logo-cc.png"
+            alt="Connecticut Creative"
+            className="h-8 w-auto"  // adjust h-# to tweak height
+          />
+        </a>
+      </div>
+
+      {/* Navigation links on the right */}
       <div className="space-x-6">
-        <a href="#services" className="text-white hover:underline">Services</a>
-        <a href="#work" className="text-white hover:underline">Work</a>
-        <a href="#about" className="text-white hover:underline">About</a>
-        <a href="#contact" className="text-white hover:underline">Contact</a>
+        <a href="#services" className="text-white hover:underline">
+          Services
+        </a>
+        <a href="#work" className="text-white hover:underline">
+          Work
+        </a>
+        <a href="#about" className="text-white hover:underline">
+          About
+        </a>
+        <a href="#contact" className="text-white hover:underline">
+          Contact
+        </a>
       </div>
     </nav>
   );
