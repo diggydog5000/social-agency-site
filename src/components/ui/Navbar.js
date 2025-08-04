4a7d39d3cda3c6a04afd6c66bba47f263adb6a9a
diggydog5000 <howardbhenry2@gmail.com>
2025-08-04 13:17:32 -0400

    Contact US Change

--- src/components/ui/Navbar.js
+++ src/components/ui/Navbar.js
@@ -30,7 +30,7 @@ export function Navbar() {
         </Link>
 
         <Link to="/contact" className="text-white hover:underline">
-          CONTACT
+          CONTACT US
         </Link>
       </div>
     </nav>
